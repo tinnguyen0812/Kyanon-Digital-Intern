bebb5631ab2c6e86a35b549ce9e6e278b46d2f97
tinnguyen0812 <51200795+tinnguyen0812@users.noreply.github.com>
2022-03-09 10:11:59 +0700

    .

---
+++ lap1.js
@@ -0,0 +1,47 @@
+var fs = require("fs");
+const prompt = require('prompt')
+var buf = new Buffer(1024);
+
+prompt.start()
+prompt.get(['path','content'],function(err,res){
+    if(err){
+        console.error(err)
+    }
+    fs.open(res.path,'r+',function(err,fd){
+        if(err){
+            return console.error(err)
+        }
+        fs.read(fd,buf,0,buf.length,0,function(err,bytes){
+            if(err){
+                console.error(err)
+            }
+            if(bytes>0){
+                console.log("content before write:")
+                console.log(buf.slice(0,bytes).toString());
+            }
+        });
+        fs.writeFile(res.path,res.content,{flag:'a+'},function(err){
+            if(err){
+                console.error(err)
+            }
+            console.log('write data success');
+
+            fs.read(fd,buf,0,buf.length,0,function(err,bytes){
+                if(err){
+                    console.error(err)
+                }
+                if(bytes>0){
+                    console.log("Content after write:")
+                    console.log(buf.slice(0,bytes).toString());
+                }
+                fs.close(fd, function(err){
+                    if (err){
+                       console.log(err);
+                    } 
+                 });
+            });
+
+        })
+
+    });
+})
---
+++ lap2.js
@@ -0,0 +1,34 @@
+const request = require('request')
+var fs = require("fs");
+const prompt = require('prompt');
+var path='content_from_page.txt'
+var url = ''
+
+prompt.start()
+prompt.get(['path'],function(err,res){
+    if(err){
+        console.error(err)
+    }
+    url=res.path;
+    request(url,function(err,res,body){
+        if(err){
+            console.error(err)
+        }
+        fs.open(path,'r+',function(err,fd){
+            if(err){
+                return console.error(err)
+            }
+            fs.writeFile(path,body,{flag:'a+'},function(err){
+                if(err){
+                    console.error(err)
+                }
+                console.log('write data success');
+                fs.close(fd, function(err){
+                    if (err){
+                    console.log(err);
+                    } 
+                });
+            })
+        });
+    })
+})
